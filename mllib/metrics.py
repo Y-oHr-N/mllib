6b4b6f7c390058481374c22551f4ead63b20f50c
Y-oHr-N <kon.y.ohr.n@gmail.com>
2019-02-01 02:13:05 +0900

    add a mean_absolute_percentage_error function

--- mllib/metrics.py
+++ mllib/metrics.py
@@ -1,6 +1,32 @@
 import numpy as np
 from sklearn.metrics import mean_squared_error, mean_squared_log_error
 from sklearn.metrics.regression import _check_reg_targets
+from sklearn.utils.validation import check_consistent_length
+
+
+def mean_absolute_percentage_error(
+    y_true,
+    y_pred,
+    sample_weight=None,
+    multioutput='uniform_average'
+):
+    _, y_true, y_pred, multioutput = _check_reg_targets(
+        y_true,
+        y_pred,
+        multioutput
+    )
+
+    check_consistent_length(y_true, y_pred, sample_weight)
+
+    ape = 100.0 * np.abs((y_true - y_pred) / y_true)
+    mape = np.average(ape, axis=0, weights=sample_weight)
+
+    if multioutput == 'raw_values':
+        return mape
+    elif multioutput == 'uniform_average':
+        multioutput = None
+
+    return np.average(mape, weights=multioutput)
 
 
 def root_mean_squared_error(
