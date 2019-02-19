e2f27c71aa90834e79124dfa7e85673f2716adae
Y-oHr-N <kon.y.ohr.n@gmail.com>
2019-02-20 02:38:30 +0900

    fix a mean_absolute_percentage_error function

--- mllib/metrics.py
+++ mllib/metrics.py
@@ -1,7 +1,8 @@
 import numpy as np
 from sklearn.metrics import mean_squared_error, mean_squared_log_error
 from sklearn.metrics.regression import _check_reg_targets
-from sklearn.utils.validation import check_consistent_length
+
+from .utils import check_sample_weight
 
 
 def mean_absolute_percentage_error(
@@ -10,16 +11,33 @@ def mean_absolute_percentage_error(
     sample_weight=None,
     multioutput='uniform_average'
 ):
+    """Mean Absolute Percentage Error (MAPE).
+
+    Examples
+    --------
+    >>> from mllib.metrics import mean_absolute_percentage_error
+    >>> y_true = [3, -0.5, 2, 7]
+    >>> y_pred = [2.5, 0.0, 2, 8]
+    >>> mean_absolute_percentage_error(y_true, y_pred)
+    32.738...
+    """
+
     _, y_true, y_pred, multioutput = _check_reg_targets(
         y_true,
         y_pred,
         multioutput
     )
 
-    check_consistent_length(y_true, y_pred, sample_weight)
+    if np.any(y_true == 0.0):
+        raise ValueError('y_true must not include zeros')
 
-    ape = 100.0 * np.abs((y_true - y_pred) / y_true)
-    mape = np.average(ape, axis=0, weights=sample_weight)
+    n_samples, _ = y_true.shape
+    sample_weight = check_sample_weight(sample_weight, n_samples)
+    mape = 100.0 * np.average(
+        np.abs((y_true - y_pred) / y_true),
+        axis=0,
+        weights=sample_weight
+    )
 
     if multioutput == 'raw_values':
         return mape
