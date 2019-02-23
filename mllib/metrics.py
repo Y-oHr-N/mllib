5a920ec6c466119e3c2b7f42b1c8f52f7c2e1d19
Y-oHr-N <kon.y.ohr.n@gmail.com>
2019-02-24 01:48:16 +0900

    add a mean_arctangent_absolute_percentage_error function

--- mllib/metrics.py
+++ mllib/metrics.py
@@ -5,6 +5,35 @@ from sklearn.metrics.regression import _check_reg_targets
 from .utils import check_sample_weight
 
 
+def mean_arctangent_absolute_percentage_error(
+    y_true,
+    y_pred,
+    sample_weight=None,
+    multioutput='uniform_average'
+):
+    _, y_true, y_pred, multioutput = _check_reg_targets(
+        y_true,
+        y_pred,
+        multioutput
+    )
+    n_samples, _ = y_true.shape
+    sample_weight = check_sample_weight(sample_weight, n_samples)
+
+    with np.errstate(divide='ignore', invalid='ignore'):
+        ape = np.abs((y_true - y_pred) / y_true)
+
+    ape = np.nan_to_num(ape)
+    aape = np.arctan(ape)
+    maape = np.average(aape, axis=0, weights=sample_weight)
+
+    if multioutput == 'raw_values':
+        return maape
+    elif multioutput == 'uniform_average':
+        multioutput = None
+
+    return np.average(maape, weights=multioutput)
+
+
 def mean_absolute_percentage_error(
     y_true,
     y_pred,
