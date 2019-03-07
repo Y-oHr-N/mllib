1a8ee01052dbc2a49faff35e9905b643ce1c8220
Y-oHr-N <kon.y.ohr.n@gmail.com>
2019-03-08 00:55:26 +0900

    add a make_mixed_transformer function

--- mllib/preprocessing.py
+++ mllib/preprocessing.py
@@ -1,5 +1,13 @@
 import numpy as np
-from sklearn.preprocessing import FunctionTransformer
+from sklearn.compose import make_column_transformer
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.impute import MissingIndicator, SimpleImputer
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import (
+    FunctionTransformer,
+    OneHotEncoder,
+    StandardScaler
+)
 
 
 def affine(X, A=1.0, b=0.0, inverse=False):
@@ -17,6 +25,25 @@ def affine(X, A=1.0, b=0.0, inverse=False):
             return X * A + b
         else:
             return X @ A + b
+
+
+def make_mixed_transformer(categorical_feature_names, numerical_feature_names):
+    categorical_transformer = make_pipeline(
+        SimpleImputer(fill_value='missing', strategy='constant'),
+        OneHotEncoder(handle_unknown='ignore')
+    )
+
+    numerical_transformer = make_union(
+        make_pipeline(SimpleImputer(), StandardScaler()),
+        MissingIndicator(sparse=True)
+    )
+
+    mixed_transformer = make_column_transformer(
+        (categorical_transformer, categorical_feature_names),
+        (numerical_transformer, numerical_feature_names)
+    )
+
+    return make_pipeline(mixed_transformer, VarianceThreshold())
 
 
 class Affine(FunctionTransformer):
