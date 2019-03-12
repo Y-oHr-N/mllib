f1335ae026ded5092275f98e60833a3001e0f386
Y-oHr-N <kon.y.ohr.n@gmail.com>
2019-03-13 02:31:23 +0900

    add a SplittedEstimator class

--- mllib/ensemble.py
+++ mllib/ensemble.py
@@ -4,7 +4,13 @@ from abc import ABC, abstractmethod
 import numpy as np
 from scipy import stats
 from sklearn.base import BaseEstimator, ClassifierMixin, RegressorMixin, clone
-from sklearn.utils import check_random_state
+from sklearn.utils import (
+    check_X_y,
+    check_array,
+    check_consistent_length,
+    check_random_state,
+    column_or_1d
+)
 from sklearn.utils.validation import check_is_fitted
 
 from .utils import is_estimator
@@ -146,3 +152,94 @@ class RandomSeedAveragingClassifier(BaseRandomSeedAveraging, ClassifierMixin):
             mode, _ = stats.mode(predictions, axis=1)
 
         return np.ravel(mode)
+
+
+class SplittedEstimator:
+    @property
+    def _estimator_type(self):
+        return self.base_estimator._estimator_type
+
+    @property
+    def get_params(self):
+        return self.base_estimator.get_params
+
+    @property
+    def set_params(self):
+        return self.base_estimator.set_params
+
+    def __init__(self, base_estimator):
+        self.base_estimator = base_estimator
+
+    def _check_is_fitted(self):
+        check_is_fitted(self, ['estimators_', 'unique_groups_'])
+
+    def _check_params(self):
+        if not is_estimator(self.base_estimator):
+            raise ValueError(
+                f'base_estimator must be a scikit-learn estimator'
+            )
+
+    def fit(self, X, y, by, **fit_params):
+        X, y = check_X_y(X, y, estimator=self)
+        by = column_or_1d(by)
+
+        check_consistent_length(X, by)
+
+        self.estimators_ = []
+        self.unique_groups_ = np.unique(by)
+
+        for i in self.unique_groups_:
+            e = clone(self.base_estimator)
+            is_train = by == i
+
+            e.fit(X[is_train], y[is_train], **fit_params)
+
+            self.estimators_.append(e)
+
+        return self
+
+    def predict(self, X, by):
+        self._check_is_fitted()
+
+        X = check_array(X, estimator=self)
+        by = column_or_1d(by)
+
+        check_consistent_length(X, by)
+
+        is_in = np.isin(by, self.unique_groups_)
+
+        if np.any(~is_in):
+            raise ValueError(f'unknown group labels are included')
+
+        y_pred = np.empty_like(by)
+
+        for i, e in zip(self.unique_groups_, self.estimators_):
+            is_test = by == i
+
+            if np.sum(is_test) > 0:
+                y_pred[is_test] = e.predict(X[is_test])
+
+        return y_pred
+
+    def score(self, X, y, by):
+        self._check_is_fitted()
+
+        X, y = check_X_y(X, y, estimator=self)
+        by = column_or_1d(by)
+
+        check_consistent_length(X, by)
+
+        is_in = np.isin(by, self.unique_groups_)
+
+        if np.any(~is_in):
+            raise ValueError(f'unknown group labels are included')
+
+        y_score = np.empty_like(by)
+
+        for i, e in zip(self.unique_groups_, self.estimators_):
+            is_test = by == i
+
+            if np.sum(is_test) > 0:
+                y_score[is_test] = e.score(X[is_test])
+
+        return y_score
